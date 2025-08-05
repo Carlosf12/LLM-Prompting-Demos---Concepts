5e9f3c78100b78891b0dc9a2b719c564f247cacd
Carlosf12 <carlosfigueira1212@gmail.com>
2025-08-04 23:38:40 -0400

    Implement Principle 2, Tactic 1: Specify steps for complex tasks

--- prompt_demo.py
+++ prompt_demo.py
@@ -27,7 +27,7 @@ def get_completion(prompt, model="llama3"):
 # 1 = Tactic 1 (Delimiters)
 # 2 = Tactic 2 (Structured Output)
 # 3 = Tactic 3 (Conditional Logic)
-demo_tactic = 4
+demo_tactic = 5
 
 # --- Prompting Principles Demos ---
 
@@ -152,4 +152,62 @@ the most intricate tapestry begins with a solitary thread.
 <child>: Teach me about resilience.
 """
     response = get_completion(prompt)
+    print(response)
+elif demo_tactic == 5:
+    print("--- Running Principle 2: Give the model time to 'think' ---")
+    print("--- Tactic 1: Specify the steps required to complete a task ---")
+
+    text = f"""
+In a charming village, siblings Jack and Jill set out on \
+a quest to fetch water from a hilltop \
+well. As they climbed, singing joyfully, misfortune \
+struck—Jack tripped on a stone and tumbled \
+down the hill, with Jill following suit. \
+Though slightly battered, the pair returned home to \
+comforting embraces. Despite the mishap, \
+their adventurous spirits remained undimmed, and they \
+continued exploring with delight.
+"""
+    # example 1
+    prompt_1 = f"""
+Perform the following actions:
+1 - Summarize the following text delimited by triple \
+backticks with 1 sentence.
+2 - Translate the summary into French.
+3 - List each name in the French summary.
+4 - Output a json object that contains the following \
+keys: french_summary, num_names.
+​
+Separate your answers with line breaks.
+​
+Text:
+```{text}```
+"""
+    response = get_completion(prompt_1)
+    print("Completion for prompt 1:")
+    print(response)
+
+    print("\n" + "=" * 20 + "\n") # Separator for clarity
+
+    # example 2
+    prompt_2 = f"""
+Your task is to perform the following actions:
+1 - Summarize the following text delimited by
+  <> with 1 sentence.
+2 - Translate the summary into French.
+3 - List each name in the French summary.
+4 - Output a json object that contains the
+  following keys: french_summary, num_names.
+​
+Use the following format:
+Text: <text to summarize>
+Summary: <summary>
+Translation: <summary translation>
+Names: <list of names in summary>
+Output JSON: <json with summary and num_names>
+​
+Text: <{text}>
+"""
+    response = get_completion(prompt_2)
+    print("Completion for prompt 2:")
     print(response)
